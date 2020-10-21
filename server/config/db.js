f851b190c60eda0a06c01666ccb07482b5a8f3aa
Wooram Moon <wpdl555@naver.com>
2020-10-21 19:05:34 -0400

    updated db URI to work with MongDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost:27017/contacts"
+    //"URI": "mongodb://localhost:27017/contacts"
+    "URI": "mongodb+srv://wooram:mxYJsvlNFrtFuQvM@mongodbserver.ksn3i.mongodb.net/component_store?retryWrites=true&w=majority"
 }
